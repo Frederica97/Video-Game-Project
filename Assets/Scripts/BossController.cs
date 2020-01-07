e13f5c6ae5d017ff2b3270d38fddff1c01be826e
Zinan Chi <31479492+Frederica97@users.noreply.github.com>
2020-01-07 13:54:24 -0600

    0108

---
+++ Assets/Scripts/BossController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossController : MonoBehaviour
+{
+
+    private GameObject target;
+    private Vector3 dir;
+    private Rigidbody2D rigidbody;
+    public float speed;
+    public GameObject navigator = null;
+    public float damage = 0.10f;
+    public int bounceBack = 100;
+    //public float health = 100f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        target = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //if (health <= 0) Destroy(gameObject);
+        if (navigator == null) dir =  target.transform.position - transform.position;
+        else dir = navigator.GetComponent<Navigator>().find_dir(transform.position, target.transform.position);
+        float angle = Mathf.Atan2(dir.y, dir.x) * Mathf.Rad2Deg;
+        rigidbody.AddForce(dir * Mathf.Min(speed, speed - rigidbody.velocity.magnitude));
+        rigidbody.MoveRotation(angle - 180);
+        rigidbody.velocity = Vector2.ClampMagnitude(rigidbody.velocity, speed);
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        //if (collision.gameObject.CompareTag("Projectile"))
+        //{
+        //    health -= 5f;
+        //}
+        if (collision.gameObject.GetComponent<PlayerController>())
+        {
+            PlayerController.instance.oxygen -= damage;
+            rigidbody.AddForce(-((target.transform.position - transform.position) * Mathf.Min(speed, speed - rigidbody.velocity.magnitude))*bounceBack);
+        }
+    }
+}
---
+++ Assets/Scripts/SoundManager.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SoundManager : MonoBehaviour
+{
+    public static SoundManager instance;
+    //Outlets
+    AudioSource audioSource;
+    public AudioClip getKeySound;
+  
+    // Start is called before the first frame update
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            instance = this;
+        }
+    }
+    void Start()
+    {
+        DontDestroyOnLoad(gameObject);
+        audioSource = GetComponent<AudioSource>();
+    }
+    public void PlaySoundGetKey()
+    {
+        audioSource.PlayOneShot(getKeySound);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
